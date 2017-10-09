05c28b162a645f6a1452ac153346a887ad384051
Jared Fitton <jaredfitton@gmail.com>
2017-10-09 09:32:34 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -7,8 +7,16 @@ def render_main():
     return render_template('home.html')
 
 @app.route("/response")
-def render_response():
-    
+def render_response(): 
+    color = request.args['color']
+    # The request object stores information about the request sent to the server.
+    # args is a MultiDict (like a dictionary but can have multiple values for the same keys
+    # The information in args is visible in the url for the page being requested (ex. .../response?color=blue)
+    if color == 'blue':
+        reply = "That's my favorite color, too!"
+    else:
+        reply = "Cool, my favorite color is blue."
+    return render_template('response.html', response = reply)
     
 if __name__=="__main__":
     app.run(debug=False, port=54321)
